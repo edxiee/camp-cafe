d4da24bffbea0a4df9bac7f6e30ffeea79b5b5ac
Javsss._. <qjespira@tip.edu.ph>
2025-10-30 00:52:16 +0000

    html inside ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,11 +3,52 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [IonicModule, FormsModule, CommonModule],
-  templateUrl: './login.page.html',
+  template: `
+  <ion-content>
+      <div class="container">
+        <img src="assets/logo.png" alt="Camp Café Logo" class="logo" />
+
+        <div class="login-box">
+          <h2>LOGIN</h2>
+
+          <ion-input
+            type="text"
+            placeholder="Username"
+            [(ngModel)]="username"
+            class="input-field"
+          ></ion-input>
+
+          <ion-input
+            [type]="showPassword ? 'text' : 'password'"
+            placeholder="Password"
+            [(ngModel)]="password"
+            class="input-field"
+          ></ion-input>
+
+          <ion-button expand="block" class="signin-btn" (click)="onLogin()">
+            Sign In
+          </ion-button>
+
+          <div class="options">
+            <div class="left">
+              <ion-checkbox [(ngModel)]="showPassword" class="checkbox"></ion-checkbox>
+              <span>Show password</span>
+            </div>
+            <a href="#" class="forgot">Forgot password?</a>
+          </div>
+        </div>
+
+        <p class="bottom-text">
+          Not a member yet? <a routerLink="/signup" class="link">Sign up now!</a>
+        </p>
+      </div>
+    </ion-content>
+`,
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
