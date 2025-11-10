50d13f830523b4467f298b431658a42461a96fb2
Javszs <qjespira@tip.edu.ph>
2025-11-10 16:18:08 +0800

    added developers in aboutApp || Profile

--- src/app/tab-menu/profile/aboutApp/aboutApp.modal.ts
+++ src/app/tab-menu/profile/aboutApp/aboutApp.modal.ts
@@ -24,6 +24,62 @@ import { ModalController } from '@ionic/angular';
       your drink faster and more convenient for busy students and professionals.
     </p>
   </div>
+
+  <ion-card>
+  <ion-card-header>
+    <ion-card-title>Developers</ion-card-title>
+    <ion-card-subtitle>Camp Café</ion-card-subtitle>
+  </ion-card-header>
+  <ion-card-content>
+    <ion-list>
+      <ion-item class="card-developer">
+        <ion-thumbnail slot="start">
+          <img src="assets/images/developers/ed.png" />
+        </ion-thumbnail>
+        <ion-label><span class="span2">Canilang, Carl </span><br> 
+        <span class="span1">Developer, Made PPT, and documentation??.</span>
+        </ion-label>
+      </ion-item>
+
+      <ion-item class="card-developer">
+        <ion-thumbnail slot="start">
+          <img src="assets/images/developers/jayvee.jpg" />
+        </ion-thumbnail>
+        <ion-label><span class="span2">Espira, Jayve L. </span><br> 
+        <span class="span1">Developer, Made Profile page, and backend.</span>
+        </ion-label>
+      </ion-item>
+
+      <ion-item class="card-developer">
+        <ion-thumbnail slot="start">
+          <img src="assets/images/developers/ayvan.jpg" />
+        </ion-thumbnail>
+        <ion-label><span class="span2">Lopez, Ayvan T. </span><br> 
+        <span class="span1">Main developer, Made Products page, Admin, and backend.</span>
+        </ion-label> 
+      </ion-item>
+
+      <ion-item class="card-developer">
+        <ion-thumbnail slot="start">
+          <img src="assets/images/developers/ed.png" />
+        </ion-thumbnail>
+        <ion-label><span class="span2">Rentoy, Jan Carlo L.</span><br> 
+        <span class="span1">Developer, Made Cart page, and UI/UX.</span>
+        </ion-label> 
+      </ion-item>
+
+      <ion-item lines="none" class="card-developer">
+        <ion-thumbnail slot="start">
+          <img src="assets/images/developers/ed.png" />
+        </ion-thumbnail>
+        <ion-label><span class="span2">Villanueva, Ed Christian F.</span><br> 
+        <span class="span1">Developer, Made Login page, and documentation.</span>
+        </ion-label>
+      </ion-item>
+    </ion-list>
+  </ion-card-content>
+</ion-card>
+
 </ion-content>
   `,
   
